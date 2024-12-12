85cab89fd71199333886e72bfc1b76f7643bd882
vhle12 <158373104+vhle12@users.noreply.github.com>
2024-12-11 17:36:18 -0800

    allow different start and end times

--- engine/src/main/kotlin/com/maiplace/CalendarDay.kt
+++ engine/src/main/kotlin/com/maiplace/CalendarDay.kt
@@ -3,14 +3,19 @@ package com.maiplace
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalTime
 
-class CalendarDay(private val date: LocalDate) {
-    companion object {
-        private val defaultStart = LocalTime(4, 0)
-        private val defaultEnd = LocalTime(0, 0)
-    }
+class CalendarDay(
+    private val date: LocalDate,
+    start: LocalTime = LocalTime(4, 0),
+    end: LocalTime = LocalTime(0, 0)
+) {
+    private val timeSlots = listOf(TimeSlot(start, end))
 
-    constructor(dateString: String): this(LocalDate.parse(dateString))
+    constructor(
+        dateString: String,
+        start: LocalTime = LocalTime(4, 0),
+        end: LocalTime = LocalTime(0, 0)
+    ): this(LocalDate.parse(dateString), start, end)
 
     fun timeSlots(filter: Filter = Filter.TrueFilter) =
-        if (filter isMetBy date) listOf(TimeSlot(defaultStart, defaultEnd)) else emptyList()
+        if (filter isMetBy date) timeSlots else emptyList()
 }
