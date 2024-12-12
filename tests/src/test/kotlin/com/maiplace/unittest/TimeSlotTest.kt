6963a4b2c828a307b294c7c42e3b9e9ff6403963
vhle12 <158373104+vhle12@users.noreply.github.com>
2024-12-11 18:08:45 -0800

    blocking for time slots

--- tests/src/test/kotlin/com/maiplace/unittest/TimeSlotTest.kt
+++ tests/src/test/kotlin/com/maiplace/unittest/TimeSlotTest.kt
@@ -21,6 +21,14 @@ class TimeSlotTest {
         assertEquals(1, listOf(timeSlot(10, 12), timeSlot(10, 12)).toSet().size)
     }
 
+    @Test
+    fun blocking() {
+        assertEquals(emptyList<TimeSlot>(), timeSlot(10, 12).block(timeSlot(10, 12)))
+        assertEquals(listOf(timeSlot(11, 12)), timeSlot(10, 12).block(timeSlot(10, 11)))
+        assertEquals(listOf(timeSlot(10, 11)), timeSlot(10, 12).block(timeSlot(11, 12)))
+        assertEquals(listOf(timeSlot(4, 10), timeSlot(12, 0)), timeSlot(4, 0).block(timeSlot(10, 12)))
+    }
+
     private fun timeSlot(startHour: Int, endHour: Int) =
         TimeSlot(LocalTime(startHour, 0), LocalTime(endHour, 0))
 }
